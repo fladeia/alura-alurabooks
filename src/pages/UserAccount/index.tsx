e8d52d3ffb5a3c410b1559b121e280e85741c9ee
fladeia <ladeiadev@gmail.com>
2022-12-28 10:05:18 -0300

    fixed UserAccount props

--- src/pages/UserAccount/index.tsx
+++ src/pages/UserAccount/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import { LastReleases } from "../../components/HighlightsBooks"
+import { HighlightsBooks } from "../../components/HighlightsBooks"
 import { UserAccountMenu } from "../../components/UserAccountMenu"
 
 export const UserAccount = () => {
@@ -8,7 +8,7 @@ export const UserAccount = () => {
       <h2 className="font-bold text-white text-lg leading-7 text-center mb-20 py-4 bg-gradient-to-r from-blue-700 to-blue-200">Minha conta</h2>
       <UserAccountMenu />
       <Outlet />
-      {/* <LastReleases /> */}
+      {/* <HighlightsBooks /> */}
     </>
   )
 }
